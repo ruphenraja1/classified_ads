46c5a1e9bb051aa23b2f019fcc3942ec1ab7a00a
roobanraj <ruphenraja1@gmail.com>
2026-04-06 11:15:05 +0530

    removed BSAE_DIR in setting

--- backend/config/settings_prod.py
+++ backend/config/settings_prod.py
@@ -139,9 +139,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = BASE_DIR / 'static/'
+# STATIC_URL = BASE_DIR / 'static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL ='/static/'
+MEDIA_ROOT = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
